4d02ab036d36af27c3fa83e9bfd7e51eec5f15eb
Ángel Cruz <langelcruzlara@gmail.com>
2021-07-11 20:06:04 -0500

    feat: implementing sticky navigation

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const $overlay = document.querySelector('.overlay');
 const $btnCloseModal = document.querySelector('.btn--close-modal');
 const $btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 // const btnScrollTo = document.querySelector('.btn--scroll-to');
-// const section1 = document.querySelector('#section--1');
+const $section1 = document.querySelector('#section--1');
 const $tabs = document.querySelectorAll('.operations__tab');
 const $tabsContainer = document.querySelector('.operations__tab-container');
 const $tabsContent = document.querySelectorAll('.operations__content');
@@ -81,3 +81,10 @@ const handleHover = function (e) {
 
 $nav.addEventListener('mouseover', handleHover.bind(0.5));
 $nav.addEventListener('mouseout', handleHover.bind(1));
+
+const initialCords = $section1.getBoundingClientRect();
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > initialCords.top) $nav.classList.add('sticky');
+  else $nav.classList.remove('sticky');
+});
